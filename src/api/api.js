1bf36beea245ba6ff4c54ab9291a8835f0387176
abinav <autocashclicks@gmail.com>
2026-07-23 22:15:03 +0530

    Update backend URL

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://roster-achievement-belief-capable.trycloudflare.com",
+  baseURL: "https://perspectives-members-wave-closure.trycloudflare.com",
 });
 
 export default api;
